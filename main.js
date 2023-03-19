23cbca8942e083374947206e723bd39544c1b0f3
User <keyloogerpy2022@gmail.com>
2023-03-19 07:33:10 +0300

    final

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 function openMenu(){
     document.getElementById("second_list").style.display = "block";
-    document.getElementById("sub_list").style.width = "50%";
-    document.getElementById("open-menu").style.display = "none";
 }
 function closeMenu(){
     document.getElementById("second_list").style.display = "none";
-    document.getElementById("sub_list").style.width = "0";
-    document.getElementById("open-menu").style.display = "none";
 }
